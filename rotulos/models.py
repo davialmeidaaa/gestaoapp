2d10a16726fe2cf445888c4b58ae9aa6e2c92879
Davi Almeida <davi.almeida@iebtinnovation.com>
2023-08-04 16:45:13 -0300

    adicionando logica de status de rotulos

--- rotulos/models.py
+++ rotulos/models.py
@@ -12,4 +12,14 @@ class Rotulo(models.Model):
     valor_cobrado = models.DecimalField(max_digits=10, decimal_places=2)
     valor_recebido = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     documentacao = models.BooleanField(default=False)
-    
+
+    @property
+    def status(self):
+        if self.documentacao and self.valor_recebido is None and self.data_entrega is None:
+            return "Confeccionar"
+        elif not self.documentacao and self.valor_recebido is None and self.data_entrega is None:
+            return "Pendente"
+        elif self.documentacao and self.valor_recebido is not None and self.data_entrega is None:
+            return "Entregar"
+        elif self.documentacao and self.valor_recebido is not None and self.data_entrega is not None:
+            return "Entregue"
--- rotulos/views.py
+++ rotulos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from rotulos.forms import RotuloForm
+from rotulos.models import Rotulo
 
 
 def adicionar_rotulo(request):
@@ -22,4 +23,23 @@ def adicionar_rotulo(request):
 
 def listar_rotulo(request):
     
-    return render(request, 'rotulos/listar_rotulo.html')
+    rotulos = Rotulo.objects.all().order_by('id')
+    
+    for rotulo in rotulos:
+        if rotulo.valor_cobrado is None:
+            rotulo.valor_cobrado_formatado = "-"
+        else:
+            rotulo.valor_cobrado_formatado = "R$ {:,.2f}".format(rotulo.valor_cobrado).replace(",", "X").replace(".", ",").replace("X", ".")
+
+        if rotulo.valor_recebido is None:
+            rotulo.valor_recebido_formatado = "-"
+        else:
+            rotulo.valor_recebido_formatado = "R$ {:,.2f}".format(rotulo.valor_recebido).replace(",", "X").replace(".", ",").replace("X", ".")
+        
+    context = {
+        'rotulos': rotulos,
+    }
+    
+    return render(request, 'rotulos/listar_rotulo.html', context)
+
+
